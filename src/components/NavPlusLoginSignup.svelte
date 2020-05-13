dc02cc532b25a65326cb529c4b54c82b24192fa7
jithusnair <work.jithus@gmail.com>
2020-05-13 22:57:18 +0530

    add new component merging nav + login + signup

---
+++ src/components/NavPlusLoginSignup.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Nav from '../components/nav/Nav.svelte';
+    import SignUp from '../components/signinandup/SignUp.svelte';
+    import Login from '../components/signinandup/Login.svelte';
+
+    export let displayLogIn = false;
+    export let displaySignUp = false;
+</script>
+
+<Nav on:navlogin/>
+
+<Login 
+    display = {displayLogIn}
+    on:signupopen
+    on:loginclose
+/>
+
+<SignUp
+    display = {displaySignUp}
+    on:loginopen
+    on:signupclose
+/>
