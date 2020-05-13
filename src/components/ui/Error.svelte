4cdedbed66205f51c229f068bd1a08a2a62d8ffc
jithusnair <work.jithus@gmail.com>
2020-05-13 16:23:55 +0530

    add error message UI component

---
+++ src/components/ui/Error.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { fade } from 'svelte/transition';
+
+    export let showErr = false;
+
+    $:  if(showErr) {
+        setTimeout(() => {
+            showErr = false;
+        }, 2000);
+    }
+</script>
+
+<style>
+    .error{
+		color: #D8000C;
+        background-color: #FFBABA;
+        border: 1px solid;
+        border-radius: 1rem;
+        margin: 0px;
+        padding: 0px 10px;
+        width: 100%;
+        text-align: center;
+    }
+</style>
+
+{#if showErr}
+    <div transition:fade={{duration:500}} class="error">
+        <slot>
+        </slot>
+    </div>
+{/if}
