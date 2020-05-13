941d90313b93253574b509c9dbea877fe0570c30
jithusnair <work.jithus@gmail.com>
2020-05-13 15:14:51 +0530

    add modal UI background component

---
+++ src/components/ui/Modal.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { fade } from 'svelte/transition';
+    
+    export let showFadeBackground = true;
+</script>
+
+<style>
+    .background {
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.2);
+        position:fixed;
+        top: 0;
+        left:0;
+        z-index: 1002;
+    }
+</style>
+
+{#if showFadeBackground}
+    <div transition:fade={{duration: 500}} class="background">
+        <slot></slot>
+    </div>
+{/if}
