704816f06e6c39e5cb928ebe7117ebad6a8d470d
jithusnair <work.jithus@gmail.com>
2020-05-14 16:11:04 +0530

    changes loading spinner

--- src/components/signinandup/SignUp.svelte
+++ src/components/signinandup/SignUp.svelte
@@ -154,6 +154,17 @@
         z-index: 1003;
     }
 
+    .loading {
+        z-index: 1005;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background: rgba(0, 0, 0, 0.2);
+        width: 100%;
+        height: 100%;
+        border-radius: inherit;
+    }
+
     .signUp form button {
         margin-top: 1rem;
     }
