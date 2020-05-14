00b137a2f9df854b98238a280819476079ab2ab5
jithusnair <work.jithus@gmail.com>
2020-05-14 16:14:41 +0530

    connects signup and signupresults

--- src/components/NavPlusLoginSignup.svelte
+++ src/components/NavPlusLoginSignup.svelte
@@ -2,9 +2,17 @@
     import Nav from '../components/nav/Nav.svelte';
     import SignUp from '../components/signinandup/SignUp.svelte';
     import Login from '../components/signinandup/Login.svelte';
+    import SignUpResults from '../components/signinandup/SignUpResults.svelte';
+    
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher()
 
     export let displayLogIn = false;
     export let displaySignUp = false;
+
+    let displaySignUpResults = false;
+    let signUpResults;
 </script>
 
 <Nav on:navlogin/>
@@ -19,4 +27,15 @@
     display = {displaySignUp}
     on:loginopen
     on:signupclose
+    on:signupsuccess = {(event) => {
+        signUpResults = event.detail;
+        displaySignUpResults = true;
+        }
+    }
+/>
+
+<SignUpResults
+    display = {displaySignUpResults}
+    {signUpResults}
+    on:close={() => displaySignUpResults = false}
 />
