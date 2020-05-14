7c1139a7fabb6b5d944f44a10acffc0bf3442612
jithusnair <work.jithus@gmail.com>
2020-05-15 00:12:20 +0530

    adds forgot token model and server helper route

---
+++ src/components/signinandup/ForgotToken.svelte
@@ -0,0 +1,170 @@
+<script>
+    import Modal from '../ui/Modal.svelte';
+    import Loading from '../ui/Loading.svelte';
+    import Error from '../ui/Error.svelte';
+
+    import { scale } from 'svelte/transition';
+    
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    let forgotPassToken = '';
+
+    let forgotPassTokenErr = '';
+
+    export let forgotemailsuccess;
+
+    export let display = false;
+
+    let loading = false;
+
+    function cleanUpClose () {
+        forgotPassToken = '';
+        forgotPassTokenErr = '';
+        dispatch('forgotclose');
+    }
+
+    function forgotToken() {
+        loading = true;
+        let data = {email: forgotPassEmail, token: forgotPassToken};
+
+        fetch('user/account/forgottoken', {
+            method: 'POST',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include', 
+            body: JSON.stringify(data),
+            })
+            .then(response => {
+                loading = false;
+                return response.json()
+            })
+            .then(data => {
+                if(data.success) {
+                    cleanUpClose();
+                    dispatch('forgottokensuccess', data.msg);
+                }
+                else if(!data.success) {
+                    forgotPassTokenErr = data.err;
+                }
+            })
+            .catch((error) => {
+            console.error('Error:', error);
+            }); 
+    }
+</script>
+
+<style>
+    .loading {
+        z-index: 1005;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background: rgba(0, 0, 0, 0.2);
+        width: 100%;
+        height: 100%;
+        border-radius: inherit;
+    }
+
+    .forgotPassword {
+        width: 300px;
+        padding: 0 10px 0 10px;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        background-color: white;
+        height: 275px;
+        transform: translate(-50%, -50%);
+        border-radius: 2rem;
+        z-index: 1003;
+    }
+
+    input {
+        margin-top: 1rem;
+    }
+
+    .forgotPassword form {
+        margin: 0 auto;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .forgotPassword div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    
+    .forgotPassword div p {
+        margin: 1.5rem;
+    }
+
+    .forgotPassword h3 {
+        margin: 2rem auto 1rem auto;
+        text-align: center;
+    }
+
+    button {
+        margin-top: 1rem;
+    }
+
+    .forgotPassword div .error-message {
+        font-size: 1rem;
+        margin:0;
+    }
+
+    .closeBtn {
+        position: absolute;
+        top: 1.5rem;
+        right: 1.5rem;
+        font-size: 2rem;
+        cursor: pointer;
+    }
+
+    .error-message {
+        font-size: 1rem;
+    }
+
+    .success {
+        text-align: center;
+        font-size: 1rem;
+        font-weight: bold;
+        line-height: 1rem;
+        margin-top: 1rem;
+    }
+</style>
+
+<Modal displayModal={display}>
+    <div transition:scale={{duration: 500}} class="forgotPassword">
+        <h3>Forgot Password</h3>
+        <!-- Loading Spinner -->
+        {#if loading}
+            <div class="loading"><Loading/></div>  
+        {/if}
+        <form on:submit|preventDefault={forgotToken}>
+            <p><strong>Please type in the code below</strong></p>
+            <Error showErr={forgotPassTokenErr? true: false}>
+                <p class="error-message">{forgotPassTokenErr}</p>
+            </Error>
+            <input 
+                type='text'
+                value = {forgotPassToken}
+                placeholder="Code"
+                on:input={event => (forgotPassToken = event.target.value)}
+                />
+            <button>Next</button>
+            {#if true}
+                <p class="success">{forgotemailsuccess}</p>
+            {/if}
+            <div on:click={cleanUpClose} class="closeBtn">
+                <i class="fa fa-times" aria-hidden="true"></i>
+            </div>
+        </form>
+    </div>
+</Modal>
---
+++ src/routes/user/account/forgottoken.js
@@ -0,0 +1,94 @@
+import User from '../../../_db/user';
+import Token from '../../../_db/token';
+import nodemailer from 'nodemailer';
+import generator from 'generate-password';
+
+// check the email of the user
+
+export async function post(req, res, next) {
+    let message;
+    // Find the given token
+    Token.findOne({ token: req.body.token }, function (err, token) {
+        if (!token) {
+            message = { 
+                success: false, 
+                err: 'Invalid code. The token code entered is incorrect.' 
+            };
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(message));
+        }
+        else {
+            // If we found a token, find a matching user
+            User.findOne({ email: req.body.email }, function (err, user) {
+                if (!user) {
+                    message = { 
+                        success: false, 
+                        err: 'We were unable to find a user for this token.' 
+                    };
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify(message));
+                }
+                // set a new password and send the email
+                else {
+                    let newPassword = generator.generate({
+                        length: 10,
+                        numbers: true,
+                        uppercase: true,
+                        lowercase: true,
+                        symbols: true
+                    });
+                    user.setPassword(newPassword , function(err, user){
+                        user.save(function (err) {
+                            if (err) { 
+                                message = { 
+                                success: false, 
+                                err: 'Something went wrong. Please try again later' 
+                                } 
+                            }
+                            else {
+                                // Send the email
+                                var transporter = nodemailer.createTransport(
+                                    { 
+                                        service: 'gmail', 
+                                        auth: { user: 'nairjithus@gmail.com', pass: 'qnwxvnvdzjitidnn' } 
+                                    });
+                        
+                                let email_html = `Hello, 
+                                Your new password is ${newPassword} 
+                                You can change your password through the profile settings section.`;
+                    
+                                var mailOptions = { 
+                                    from: 'no-reply@webapplication.com', 
+                                    to: user.email, 
+                                    subject: 'Account Verification', 
+                                    text: email_html
+                                };
+                                
+                                transporter.sendMail(mailOptions, function (err) {
+                                    if (err) { 
+                                        message = { 
+                                            success: false, 
+                                            err: `Sorry, something went wrong. 
+                                            We were unable to send a new password to your email.
+                                            Mail our support for help.`
+                                        }
+                                    }
+                                    else {
+                                        message = { 
+                                            success: true, 
+                                            msg: `Successfully changed password.
+                                            The new password has been sent to ${user.email}` 
+                                        }; 
+                                    }
+                                    res.setHeader('Content-Type', 'application/json');
+                                    res.end(JSON.stringify(message));
+                                });
+                            }
+                        });
+                    })
+                    
+                }
+            });
+        }
+    });
+}
