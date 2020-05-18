98b9d6d9a71a9d5e6ea05ca7cfb66a4eaf92d337
jithusnair <work.jithus@gmail.com>
2020-05-18 17:03:23 +0530

    adds comments, adds server errors and removes bugs

--- src/routes/user/account/loginlogout.js
+++ src/routes/user/account/loginlogout.js
@@ -1,37 +1,49 @@
 import passport from 'passport';
 
+// Log In
+// ****NOTE****
+// Sending response is quite boilerplaty.
+// An async await/promise system might be better,
+// but currently not enough time to figure out how to
+// promisify passport.authenticate()
 export async function post(req, res, next) {
     let message;
     passport.authenticate('local', function(err, user, info) {
         if (err) { 
-            message = { success: false, err: err };
+            message = { success: false, serverErr: err };
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(message));
         }
-        if (!user) {
+        else if (!user) {
             message = { success: false, loginErr: info.message };
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(message));
         }
-        req.logIn(user, function(err) {
-            if (err) {
-                message = { success: false, loginErr: info.message };
-            } else {
-                
-                let user = req.user;
-				
-                let tar = JSON.parse(JSON.stringify(req.user))
-                user = {
-                    _id: tar._id.toString(), 
-                    username: tar.fullname,
-                    fullname: tar.fullname,
-                    isVerified: tar.isVerified,
-                };
-	
-                message = { success: true, user: user };
-            }
-        });
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(message));
+        else {
+            req.logIn(user, function(err) {
+                if (err) {
+                    message = { success: false, serverErr: err };
+                } else {  
+                    let user = req.user;
+                    
+                    let tar = JSON.parse(JSON.stringify(req.user))
+                    user = {
+                        _id: tar._id.toString(), 
+                        username: tar.fullname,
+                        fullname: tar.fullname,
+                        isVerified: tar.isVerified,
+                    };
+        
+                    message = { success: true, user: user };
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify(message));
+                }
+            });
+        }
       })(req, res, next);
 }
 
+// Log Out
 export async function get(req, res, next) {
     req.logout();
     let message = {success: true, user:null};
