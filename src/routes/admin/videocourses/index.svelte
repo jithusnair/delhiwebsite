40d74bf79f6f63529199a4c8fe2637710d51dfe6
jithusnair <work.jithus@gmail.com>
2020-05-21 17:12:24 +0530

    adds video course management page

---
+++ src/routes/admin/videocourses/index.svelte
@@ -0,0 +1,48 @@
+<script>
+    import PreviewCard from '../../../components/admin/video/PreviewCard.svelte';
+    import CourseNewAndEdit from '../../../components/admin/video/CourseNewAndEdit.svelte';
+
+    let data;
+</script>
+
+<style>
+    .topContainer {
+		width: 80%;
+		margin: 10rem auto 2rem auto;
+		display: flex;
+		justify-content: space-evenly;
+		border-top: grey solid 1px;
+	}
+
+	.bottomContainer {
+		width: 80%;
+		margin: 2rem auto;
+	}
+	
+	.lefthalf, .righthalf {
+		width: 50%;
+		text-align: center;
+		height: 600px;
+		border-bottom: 1px solid black; 
+	}
+
+	.righthalf {
+		border-left: 1px solid black;
+	}
+</style>
+
+<div class="topContainer">
+	<div class="lefthalf">
+		<h1>Online Courses</h1>
+		<CourseNewAndEdit/>
+	</div>
+
+	<div class="righthalf">
+		<h1>Preview</h1>
+		<div>
+			<PreviewCard {data}/>
+		</div>
+	</div>
+</div>
+
+
