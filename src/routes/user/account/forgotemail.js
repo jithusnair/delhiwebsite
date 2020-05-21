4677da5764751a014972432b7002bbc00bf312e5
jithusnair <work.jithus@gmail.com>
2020-05-21 09:54:49 +0530

    promisifies async i/o operations

--- src/routes/user/account/forgotemail.js
+++ src/routes/user/account/forgotemail.js
@@ -7,68 +7,76 @@ import generator from 'generate-password';
 
 export async function post(req, res, next) {
     let message;
+    let foundUser;
     // Find a user with the given email
-    User.findOne({ email: req.body.email }, function (err, user) {
-        if (!user) {
-            message = { 
-                success: false, 
-                err: 'We were unable to find a user with this email.' 
+    User.findOne({ email: req.body.email }).exec()
+        .then((user) => {
+            if (!user) {
+                var e = new Error('We were unable to find a user with this email.');
+                e.name = 'NoUserError';
+                throw e;
+            }
+            // generate a token and save
+            else {
+                foundUser = user;
+                let randomToken = generator.generate({
+                    length: 10,
+                    numbers: true,
+                    uppercase: true,
+                    lowercase: true,
+                    symbols: true
+                });
+                let token = new Token({ _userId: user._id, token: randomToken });
+                return token.save().exec()
+            }
+        })
+        // send the email
+        .then((token) => {
+            var transporter = nodemailer.createTransport(
+                { 
+                    service: 'gmail', 
+                    auth: { user: process.env.USERNAME, pass: process.env.PASSWORD } 
+                });
+            
+            let email_html = `Hello, 
+            Your token is ${token.token}`;
+
+            var mailOptions = { 
+                from: 'no-reply@webapplication.com', 
+                to: foundUser.email, 
+                subject: 'Reset Password', 
+                text: email_html
             };
+            
+            transporter.sendMail(mailOptions, function (err) {
+                message = { 
+                    success: true, 
+                    msg: `A token code has been sent to ${foundUser.email}. <br>
+                    Type the code in to reset password.` 
+                }; 
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(message));
+            });
+        })
+        .catch((err) => {
+            // if no user was found
+            if(err.name == 'NoUserError') {
+                message = { 
+                    success: false, 
+                    err: err.message
+                };
+            }
+            // all other potential server errors
+            else {
+                console.log(err);
+                message = { 
+                    success: false, 
+                    serverErr: 
+                    `Something went wrong on our end! 
+                    Please try again later.`
+                };
+            }
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(message));
-            
-        }
-        // send a token to email
-        else {
-            let randomToken = generator.generate({
-                length: 10,
-                numbers: true,
-                uppercase: true,
-                lowercase: true,
-                symbols: true
-            });
-            let token = new Token({ _userId: user._id, token: randomToken });
-        
-            // Save the verification token
-            token.save(function (err) {
-                if (err) { return res.status(500).send({ msg: err.message }); }
-        
-                // Send the email
-                var transporter = nodemailer.createTransport(
-                    { 
-                        service: 'gmail', 
-                        auth: { user: 'nairjithus@gmail.com', pass: 'qnwxvnvdzjitidnn' } 
-                    });
-                
-                let email_html = `Hello, 
-                Your token is ${token.token}`;
-    
-                var mailOptions = { 
-                    from: 'no-reply@webapplication.com', 
-                    to: user.email, 
-                    subject: 'Reset Password', 
-                    text: email_html
-                };
-                
-                transporter.sendMail(mailOptions, function (err) {
-                    if (err) { 
-                        message = { 
-                            success: false, 
-                            err: `Sorry, something went wrong. We were unable to send an email.
-                            Try again later.`
-                        }
-                    }
-                    else {
-                        message = { 
-                            success: true, 
-                            msg: `A token code has been sent to ${user.email}. <br>
-                            Type the code in to reset password.` 
-                        }; 
-                    }
-                    res.setHeader('Content-Type', 'application/json');
-                    res.end(JSON.stringify(message));
-                });
-            });
-        }       
-    });
+        })
 }
--- src/routes/user/account/forgottoken.js
+++ src/routes/user/account/forgottoken.js
@@ -8,87 +8,91 @@ import generator from 'generate-password';
 export async function post(req, res, next) {
     let message;
     // Find the given token
-    Token.findOne({ token: req.body.token }, function (err, token) {
-        if (!token) {
-            message = { 
-                success: false, 
-                err: 'Invalid code. The token code entered is incorrect.' 
+    Token.findOne({ token: req.body.token}).exec()
+        .then(() => {
+            if (!token) {
+                var e = new Error('Invalid code. The token code entered is incorrect');
+                e.name = 'TokenError';
+                throw e;
+            }
+            // If we found a token, find a matching user
+            else {
+                return User.findOne({ email: req.body.email }).exec()
+            }
+        })
+        .then((user)=>{
+            if (!user) {
+                var e = new Error('We were unable to find a user for this token.');
+                e.name = 'NoUserError';
+                throw e;
+            }
+            // set a new password and send the email
+            else {
+                let newPassword = generator.generate({
+                    length: 10,
+                    numbers: true,
+                    uppercase: true,
+                    lowercase: true,
+                    symbols: true
+                });
+                return user.setPassword(newPassword);
+            }
+        })
+        .then((user) => user.save().exec())
+        .then((user) => {
+            var transporter = nodemailer.createTransport(
+                { 
+                    service: 'gmail', 
+                    auth: { user: process.env.USERNAME, pass: process.env.PASSWORD } 
+                });
+    
+            let email_html = `Hello, 
+            Your new password is ${newPassword} 
+            You can change your password through the profile settings section.`;
+
+            var mailOptions = { 
+                from: 'no-reply@webapplication.com', 
+                to: user.email, 
+                subject: 'Account Verification', 
+                text: email_html
             };
+            // Send Mail
+            transporter.sendMail(mailOptions, function (err) {
+                message = { 
+                    success: true, 
+                    msg: `Successfully changed password. <br>
+                    The new password has been sent to ${user.email}` 
+                };
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(message)); 
+            });
+        })
+        .catch((err) => {
+            // If no token was found
+            if(err.name == 'TokenError') {
+                message = { 
+                    success: false, 
+                    err: err.message
+                };
+            }
+            // if for somereason the user corresponding to token does not exist
+            else if(err.name == 'NoUserError') {
+                message = { 
+                    success: false, 
+                    err: err.message
+                };
+            }
+            // all other potential server errors
+            else {
+                console.log(err);
+                message = { 
+                    success: false, 
+                    serverErr: 
+                    `Something went wrong on our end! 
+                    Please try again later.`
+                };
+            }
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(message));
-        }
-        else {
-            // If we found a token, find a matching user
-            User.findOne({ email: req.body.email }, function (err, user) {
-                if (!user) {
-                    message = { 
-                        success: false, 
-                        err: 'We were unable to find a user for this token.' 
-                    };
-                    res.setHeader('Content-Type', 'application/json');
-                    res.end(JSON.stringify(message));
-                }
-                // set a new password and send the email
-                else {
-                    let newPassword = generator.generate({
-                        length: 10,
-                        numbers: true,
-                        uppercase: true,
-                        lowercase: true,
-                        symbols: true
-                    });
-                    user.setPassword(newPassword , function(err, user){
-                        user.save(function (err) {
-                            if (err) { 
-                                message = { 
-                                success: false, 
-                                err: 'Something went wrong. Please try again later' 
-                                } 
-                            }
-                            else {
-                                // Send the email
-                                var transporter = nodemailer.createTransport(
-                                    { 
-                                        service: 'gmail', 
-                                        auth: { user: 'nairjithus@gmail.com', pass: 'qnwxvnvdzjitidnn' } 
-                                    });
-                        
-                                let email_html = `Hello, 
-                                Your new password is ${newPassword} 
-                                You can change your password through the profile settings section.`;
-                    
-                                var mailOptions = { 
-                                    from: 'no-reply@webapplication.com', 
-                                    to: user.email, 
-                                    subject: 'Account Verification', 
-                                    text: email_html
-                                };
-                                
-                                transporter.sendMail(mailOptions, function (err) {
-                                    if (err) { 
-                                        message = { 
-                                            success: false, 
-                                            err: `Sorry, something went wrong. 
-                                            We were unable to send a new password to your email.
-                                            Mail our support for help.`
-                                        }
-                                    }
-                                    else {
-                                        message = { 
-                                            success: true, 
-                                            msg: `Successfully changed password. <br>
-                                            The new password has been sent to ${user.email}` 
-                                        }; 
-                                    }
-                                    res.setHeader('Content-Type', 'application/json');
-                                    res.end(JSON.stringify(message));
-                                });
-                            }
-                        });
-                    })
-                    
-                }
-            });
-        }
-    });
+        })
 }
