b069ebe07c9e9221e3dc57e701fcc42b37436c01
jithusnair <work.jithus@gmail.com>
2020-05-22 23:38:51 +0530

    adds error message timeout

--- src/routes/admin/index.svelte
+++ src/routes/admin/index.svelte
@@ -14,6 +14,12 @@
 
     let errorMsg = '';
 
+     $:  if(errorMsg) {
+            setTimeout(() => {
+                errorMsg = false;
+            }, 10000);
+        }
+
     let loading = false;
 
     function login() {
@@ -65,10 +71,6 @@
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-    }
-    
-    h2 {
-        text-align: center;
     }
     
     form {
