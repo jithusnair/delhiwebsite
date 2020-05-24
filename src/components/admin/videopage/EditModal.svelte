1f3012b4b4bda18d96c73715074903efc09aed23
jithusnair <work.jithus@gmail.com>
2020-05-24 17:11:32 +0530

    new component to edit video details

---
+++ src/components/admin/videopage/EditModal.svelte
@@ -0,0 +1,40 @@
+<script>
+    import Modal from '../../ui/Modal.svelte';
+    import VideoNewAndEdit from './VideoNewAndEdit.svelte';
+
+    import {createEventDispatcher} from 'svelte';
+
+    import { scale } from 'svelte/transition';
+
+    const dispatch = createEventDispatcher();
+
+    export let display;
+    export let data;
+</script>
+
+<style>
+    .editCourse {
+        background: white;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        border-radius: 2rem;
+    }
+</style>
+
+<Modal displayModal = {display}>
+    <!-- 
+        Custom events other than those listed below in 
+        CourseNewAndEdit component don't need to be handled 
+        here because those events wont be triggered in 
+        edit mode anyway 
+    -->
+    <div transition:scale|local="{{duration: 400}}" class="editCourse">
+        <VideoNewAndEdit
+            editData = {data}
+            on:editSuccess={() => dispatch('reloadData')}
+            on:close
+        />
+    </div>
+</Modal>
