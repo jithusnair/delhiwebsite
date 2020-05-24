bf375d02812a924f5e84b0fc87686b6c6426ab6d
jithusnair <work.jithus@gmail.com>
2020-05-24 17:53:39 +0530

    adds each video course page route

---
+++ src/routes/admin/videocourses/[course]/index.svelte
@@ -0,0 +1,124 @@
+<script context='module'>
+    export async function preload({ params }) {
+		const { course } = params;
+		// const res = await this.fetch(`/admin/videocourses/video_crud`);
+
+		// if (res.status === 200) {
+		// 	const article = await res.json();
+		// 	return { article };
+		// }
+
+		return { course }
+	}
+</script>
+
+<script>
+	import PreviewVideo from '../../../../components/admin/videopage/PreviewVideo.svelte';
+	import VideoNewAndEdit from '../../../../components/admin/videopage/VideoNewAndEdit.svelte';
+	import VideosAdmin from '../../../../components/admin/videopage/VideosAdmin.svelte';
+
+	import { fetchWithTimeout } from '../../../../_helpers/fetchWithTimeout.js';
+
+	import { onMount } from 'svelte';
+
+	onMount(()=>{
+		getVideos();
+	});
+
+	export let course;
+
+	let previewData;
+
+	let dbVideoData;
+
+	function getVideos() {
+		fetchWithTimeout(`/admin/videocourses/${course}/video_crud`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include', 
+        },
+        10000)
+        .then(response => {
+			return response.json()
+		})
+        .then(data => {
+            if(data.success) {
+				dbVideoData = data.data;
+            }
+			else if (data.err) {
+				// need to display GET request errors
+                // errorMsg = data.loginErr;
+            }
+        })
+        .catch((error) => {
+			// need to display GET request timeout and server errors
+            if (error.name === 'AbortError') {
+                // errorMsg = 'Server taking too long to respond! Request timed out';
+            }
+            console.error('Error:', error);
+        });
+	}
+</script>
+
+<style>
+    .topContainer {
+		width: 80%;
+		margin: 10rem auto 2rem auto;
+		display: flex;
+		justify-content: space-evenly;
+		border-top: grey solid 1px;
+	}
+
+	.bottomContainer {
+		width: 80%;
+		margin: 2rem auto;
+	}
+	
+	.lefthalf, .righthalf {
+		width: 50%;
+		text-align: center;
+		height: auto;
+		border-bottom: 1px solid black; 
+	}
+
+	.righthalf {
+		border-left: 1px solid black;
+	}
+</style>
+
+<svelte:head>
+    <link 
+        rel="stylesheet" 
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</svelte:head>
+
+<div class="topContainer">
+	<div class="lefthalf">
+		<h1>New Video</h1>
+        <VideoNewAndEdit
+			courseId = {course} 
+			on:save={()=>{
+                previewData = null;
+                getVideos();
+            }}
+            on:inputChange={(event) => previewData = event.detail}
+		/>
+	</div>
+
+	<div class="righthalf">
+		<h1>Preview</h1>
+		<div>
+			<PreviewVideo videoData={previewData}/>
+		</div>
+	</div>
+</div>
+<div class="bottomContainer">
+	<h1>Videos in Database</h1>
+    <VideosAdmin 
+		docs = {dbVideoData}
+		courseId = {course}
+		on:reloadData={getVideos}
+	/>
+</div>
