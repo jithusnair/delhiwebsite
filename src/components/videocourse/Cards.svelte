fd38d5ff7ecb9229ab4084cb94ca5e857c361516
jithusnair <work.jithus@gmail.com>
2020-05-25 19:45:41 +0530

    component to manage set of cards in grid

---
+++ src/components/videocourse/Cards.svelte
@@ -0,0 +1,42 @@
+<script>
+    import Card from './Card.svelte';
+
+    import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+    export let docs;
+</script>
+
+<style>
+    .blog-container {
+        width: 90%;
+        margin: 0 auto;
+        display: grid;
+        justify-content: center;
+        grid-gap: 5rem;
+        grid-template-columns: 300px 300px 300px;
+        grid-auto-rows: 450px;
+        grid-auto-flow: dense;
+    }
+
+    @media only screen and (max-width: 1000px) {
+        .blog-container {
+            grid-template-columns: 300px 300px;
+        }
+    }
+
+    @media only screen and (max-width: 670px) {
+        .blog-container {
+            grid-template-columns: 300px;
+        }
+    }
+</style>
+
+<div class="blog-container">
+    {#if docs}
+        {#each docs as data (data._id)}
+            <Card {data}/>
+        {/each}
+    {/if}
+</div>
