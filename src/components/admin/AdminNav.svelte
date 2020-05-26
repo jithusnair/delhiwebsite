f6bb748d796abba3a65d0dcf84f3c4ac4c696eee
jithusnair <work.jithus@gmail.com>
2020-05-26 19:54:20 +0530

    new nav component for logged in admins

---
+++ src/components/admin/AdminNav.svelte
@@ -0,0 +1,141 @@
+<script>
+  import { stores } from '@sapper/app';
+  import { goto } from '@sapper/app';
+  const { session } = stores();
+  
+  function logout() {
+        fetch('/admin/loginlogout', {
+            method: 'GET',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include', 
+            })
+            .then(response => response.json())
+            .then(data => {
+                if(data.success) {
+                    goto('/backdoor');
+                    session.set({});
+                }
+            })
+            .catch((error) => {
+            console.error('Error:', error);
+            });
+    }
+</script>
+
+<style>
+  .sidenav {
+    height: 100%;
+    width: 225px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color:  rgb(34,39,73);
+    overflow-x: hidden;
+    padding-top: 10px;
+    font-size: 1.5rem;
+    transition: width 0.5s;
+  }
+
+  .sidenav a  {
+    outline: 0;
+    border: none;
+    --moz-outline-style: none;
+  }
+
+  button::-moz-focus-inner {
+    border: 0;
+  }
+
+  .btn{
+    position: absolute;
+    bottom: 5%;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 1.5rem;
+    background: none;
+    border: 1px solid white;
+    border-radius: 3px;
+    width: 9.5rem;
+    padding: 5px 0;
+    color: white;
+    font-family: Manrope, sans-serif;
+    font-weight: 700;
+    line-height: 2rem;
+    -webkit-appearance: none;
+    text-align: center;
+    outline: none;
+    cursor: pointer;
+  }
+
+  /* Style the sidenav links and the dropdown button */
+  .sidenav a {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    color: white;
+    display: block;
+    border: none;
+    background: none;
+    width:100%;
+    text-align: left;
+    cursor: pointer;  
+  }
+
+  a {
+    margin-top: 1.7rem;
+    margin-left: 1rem;
+    margin-bottom: 1.7rem;
+  }
+
+  /* On mouse-over */
+  .sidenav a:hover {
+    color: rgb(136, 136, 247); 
+  }
+
+  #logo{
+    z-index: 999;
+    padding:1rem 2vw 1rem 0;
+    color: #fff;;
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    align-items: center;
+    margin-top: 0;
+  }
+
+  .logo-img {
+    width: 4rem;
+    margin-right: 10px;
+  }
+
+  .logo-name {
+    text-transform: uppercase;
+    color: #fff;
+  }
+
+  #nav {
+    margin-top: 1rem;
+  }
+</style>
+
+<div class="sidenav">
+  <a href="/admin" id="logo">
+    <img src="/images/nav/logo.svg" alt="" class="logo-img">
+    <h3 class="logo-name">Direction</h3>
+  </a>
+  <div id="nav">
+    <a href="/admin">
+      Home
+    </a>
+    <a href='/admin/videocourses'>
+      Manage Online Classes
+    </a>
+    <a href=''>
+      Users
+    </a>
+  </div>
+  <button on:click={logout} class="btn">Logout</button>
+</div> 
