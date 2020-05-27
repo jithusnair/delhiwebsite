c396fadb0aa348262ab93ac6e63bdd74f040c1af
jithusnair <work.jithus@gmail.com>
2020-05-27 13:12:42 +0530

    new _layout page for /onlineclass route

---
+++ src/routes/onlineclass/_layout.svelte
@@ -0,0 +1,37 @@
+<script>
+	import NavPlusLoginSignup from '../../components/NavPlusLoginSignup.svelte';
+
+	export let segment;
+
+	let displayLogIn = false;
+	let displaySignUp = false;
+</script>
+
+<style>
+	main {
+		height: 100%;		
+		padding-top: 6rem;
+	}
+</style>
+
+<main>
+	<slot></slot>
+</main>
+
+{#if segment === undefined}
+	<NavPlusLoginSignup
+		{displayLogIn}
+		{displaySignUp}
+		on:navlogin={() => displayLogIn = true}
+		on:signupopen = { () => {
+			displayLogIn = false;
+			displaySignUp = true;
+		}}
+		on:loginclose={() => displayLogIn = false}
+		on:loginopen = { () => {
+			displaySignUp = false;
+			displayLogIn = true;
+		}}
+		on:signupclose={() => displaySignUp = false}
+	/>
+{/if}
