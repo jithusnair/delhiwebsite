729ad296255ca070333a1a1d5ecf06917136575e
jithusnair <work.jithus@gmail.com>
2020-05-27 11:16:32 +0530

    design changes and show messege for no videos

--- src/components/videocourse/Videos.svelte
+++ src/components/videocourse/Videos.svelte
@@ -13,27 +13,30 @@
 
 <style>
     .blog-container {
-        width: 70%;
+        width: 90%;
         margin: 0 auto;
     }
 
     @media only screen and (max-width: 1000px) {
         .blog-container {
-            width: 80%;
+            width: 85%;
         }
     }
 
     @media only screen and (max-width: 670px) {
         .blog-container {
-            width: 90%;
+            width: 80%;
         }
     }
 </style>
 
 <div class="blog-container">
-    {#if docs}
+    {#if docs.length !=0}
         {#each docs as data (data._id)}
             <Video videoData = {data}/>
         {/each}
+    {:else}
+        <p>Videos are being uploaded by our team. 
+        Please wait, we'll have them ready soon</p>
     {/if}
 </div>
