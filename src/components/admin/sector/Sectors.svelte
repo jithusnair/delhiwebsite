c2fd27272a2f9c7024fb1d93a53c4d72d9d34c22
jithusnair <work.jithus@gmail.com>
2020-07-02 22:39:14 +0530

    adds component to display several sectors

---
+++ src/components/admin/sector/Sectors.svelte
@@ -0,0 +1,30 @@
+<script>
+    import SectorDisplayEdit from './SectorDisplayEdit.svelte';
+
+    export let sectors;
+</script>
+
+<style>
+    h3 {
+        margin-bottom: 3rem;
+    }
+
+    .sectorContainer {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, 300px);
+        grid-gap: 2rem;
+    }
+</style>
+
+<div class="container">
+    <h3>Sectors in Database</h3>
+    <div class="sectorContainer">
+        {#each sectors as sector}
+            <SectorDisplayEdit 
+                {sector}
+                on:update
+                on:delete
+            />
+        {/each}
+    </div>
+</div>
