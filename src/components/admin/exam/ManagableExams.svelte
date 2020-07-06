996da50f1cae58d1bcc0780ed45e844a39a42590
jithusnair <work.jithus@gmail.com>
2020-07-06 11:53:04 +0530

    new exams-in-db managing component

---
+++ src/components/admin/exam/ManagableExams.svelte
@@ -0,0 +1,24 @@
+<script>
+    import ExamDisplayAndEdit from './ExamDisplayAndEdit.svelte';
+
+    export let exams;
+    export let sectors;
+</script>
+
+<style>
+    .container {
+        background-color: white;
+        width: 100%;
+    }
+</style>
+
+<div class="container">
+    {#each exams as exam}
+        <ExamDisplayAndEdit 
+            {sectors} 
+            {exam}
+            on:update
+            on:delete
+        />
+    {/each}
+</div>
