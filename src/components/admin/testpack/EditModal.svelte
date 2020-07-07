ad4e51e93367fc2c9ad2820379abcf48b6bd24ca
jithusnair <work.jithus@gmail.com>
2020-07-07 20:39:52 +0530

    new testpack edit modal component

---
+++ src/components/admin/testpack/EditModal.svelte
@@ -0,0 +1,43 @@
+<script>
+    import Modal from '../../ui/Modal.svelte';
+    import TestPackNewAndEdit from './TestPackNewAndEdit.svelte';
+
+    import {createEventDispatcher} from 'svelte';
+
+    import { scale } from 'svelte/transition';
+
+    const dispatch = createEventDispatcher();
+
+    export let display;
+    export let data;
+    export let exams;
+</script>
+
+<style>
+    .editCourse {
+        background: white;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        overflow: scroll;
+        height: 550px;
+    }
+</style>
+
+<Modal displayModal = {display}>
+    <!-- 
+        Custom events other than those listed below in 
+        CourseNewAndEdit component don't need to be handled 
+        here because those events wont be triggered in 
+        edit mode anyway 
+    -->
+    <div transition:scale|local="{{duration: 400}}" class="editCourse">
+        <TestPackNewAndEdit
+            {exams}
+            editData = {data}
+            on:editSuccess={() => dispatch('reloadData')}
+            on:close
+        />
+    </div>
+</Modal>
