70f2a980a250987fef0e8749fbef5551ff64d6fb
jithusnair <work.jithus@gmail.com>
2020-07-11 15:18:03 +0530

    new edit modal component for test sets

---
+++ src/components/admin/testsets/testset/EditModal.svelte
@@ -0,0 +1,39 @@
+<script>
+    import Modal from '../../../ui/Modal.svelte';
+    import TestSetNewAndEdit from './TestSetNewAndEdit.svelte';
+
+    import {createEventDispatcher} from 'svelte';
+
+    import { scale } from 'svelte/transition';
+
+    const dispatch = createEventDispatcher();
+
+    export let display;
+    export let data;
+</script>
+
+<style>
+    .editCourse {
+        background: white;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
+
+<Modal displayModal = {display}>
+    <!-- 
+        Custom events other than those listed below in 
+        CourseNewAndEdit component don't need to be handled 
+        here because those events wont be triggered in 
+        edit mode anyway 
+    -->
+    <div transition:scale|local="{{duration: 400}}" class="editCourse">
+        <TestSetNewAndEdit
+            editData = {data}
+            on:editSuccess={() => dispatch('reloadData')}
+            on:close
+        />
+    </div>
+</Modal>
