9e06bbe9444f0dcdbf35c2950da9a2ad394cf13b
jithusnair <work.jithus@gmail.com>
2020-07-11 15:05:36 +0530

    new sections listing component

---
+++ src/components/admin/testsets/section/Sections.svelte
@@ -0,0 +1,28 @@
+<script>
+    import SectionEditAndDelete from './SectionEditAndDelete.svelte';
+    import { sectionStore } from '../../../../store/section.js';
+
+    export let testPackId;
+</script>
+
+<style>
+    h3 {
+        margin-bottom: 3rem;
+    }
+</style>
+
+<div class="container">
+    <h3>Sections in Database</h3>
+    <div class="sectorContainer">
+        {#if $sectionStore}
+            {#each $sectionStore as section}
+                <SectionEditAndDelete 
+                    {testPackId}
+                    {section}
+                    on:update
+                    on:delete
+                />
+            {/each}
+        {/if}
+    </div>
+</div>
