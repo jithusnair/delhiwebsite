c88f99c4836f58fad1992abba5d4d732d74b6c63
jithusnair <work.jithus@gmail.com>
2020-07-21 21:52:36 +0530

    new component to manage all subjects

---
+++ src/components/admin/questions/subject/Subjects.svelte
@@ -0,0 +1,30 @@
+<script>
+    import SubjectEditAndDelete from './SubjectEditAndDelete.svelte';
+    import { subjectStore } from '../../../../store/subject.js';
+
+    export let testPackId;
+    export let testSetId;
+</script>
+
+<style>
+    h3 {
+        margin-bottom: 3rem;
+    }
+</style>
+
+<div class="container">
+    <h3>Subjects in Database</h3>
+    <div class="sectorContainer">
+        {#if $subjectStore}
+            {#each $subjectStore as subject}
+                <SubjectEditAndDelete 
+                    {testPackId}
+                    {testSetId}
+                    {subject}
+                    on:update
+                    on:delete
+                />
+            {/each}
+        {/if}
+    </div>
+</div>
