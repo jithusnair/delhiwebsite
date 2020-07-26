588222da62ac60d657c220ba5d31a0fdaf6e3052
jithusnair <work.jithus@gmail.com>
2020-07-26 16:57:21 +0530

    fetches nav elements on preload

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,12 +1,49 @@
+<script context='module'>
+    export async function preload({ params, query }) {
+		const { course } = params;
+
+		let buyNow = query.buy;
+
+		let navigation;
+
+		let getError;
+
+		return await this.fetch('/nav/getNav')
+        .then(response => response.json())
+        .then(data => {
+            if(data.success) {
+				navigation = data.data
+                return {navigation};
+            }
+            else if (data.serverErr){
+                serverErr = data.serverErr;
+            }
+        })
+        .catch((error) => {
+            if (error.name === 'AbortError') {
+                saveError = 'Server taking too long to respond! Request timed out';
+            }
+            console.error('Error:', error);
+		})
+	}
+</script>
+
 <script>
 	import NavPlusLoginSignup from '../components/NavPlusLoginSignup.svelte';
 	import Footer from '../components/footer/footer.svelte'; 
 	import {fade} from 'svelte/transition';
+
+	import { fetchWithTimeout } from '../_helpers/fetchWithTimeout.js';
+    import { navigationStore } from '../store/navigation.js';
 	
 	import { stores } from '@sapper/app';
 	const { preloading } = stores();
 
 	export let segment;
+
+	export let navigation;
+
+	navigationStore.set(navigation);
 
 	let displayLogIn = false;
 	let displaySignUp = false;
@@ -27,7 +64,11 @@
 	<main transition:fade={{duration: 300}}>	
 		<slot>
 		</slot>
-		{#if segment === 'underconstruction' || segment === 'contact' || segment == 'policies'}
+		{#if 
+			segment !== 'admin' && segment !== 'user' && 
+			segment !== 'petition' && segment !== 'backdoor' &&
+			segment !== 'onlineclass' && segment
+		}
 		<NavPlusLoginSignup
 			{displayLogIn}
 			{displaySignUp}
@@ -44,7 +85,11 @@
 			on:signupclose={() => displaySignUp = false}
 		/>
 		{/if}
-		{#if !segment || segment == 'contact' || segment == 'onlineclass' || segment == 'policies'}
+		{#if 
+			segment != 'user' && segment != 'admin' &&
+			segment != 'backdoor' && segment != 'underconstruction' &&
+			segment != 'petition'
+		}
 			<Footer/>
 		{/if}
 	</main>
