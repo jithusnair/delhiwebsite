7daf9a2d3154c322714e17021052736f0dd045c6
jithusnair <work.jithus@gmail.com>
2020-07-26 17:03:32 +0530

    new api route to GET tests-details

---
+++ src/routes/examsandtests/testlist/tests.js
@@ -0,0 +1,36 @@
+import Sectors from '../../../_db/sector';
+import Exams from '../../../_db/exam';
+import TestPacks from '../../../_db/testpack';
+
+export async function get(req, res, next) {
+    let message;
+
+    let sectors;
+    let exams;
+    
+    Sectors.find({}).exec()
+    .then((docs)=> {
+        sectors = docs;
+        return Exams.find({}).select('_id examShortTitle sectorId').exec();
+    })
+    .then((docs)=> {
+        exams = docs;
+        return TestPacks.find({}).exec();
+    })
+    .then((docs)=> {        
+        message = {success: true, sectors: sectors, exams: exams, testPacks: docs};
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(message));
+    })
+    .catch((error) => {
+        console.log(error);
+        message = { 
+            success: false, 
+            serverErr: 
+            `Something went wrong. Couldn't get videos from database! 
+            Please try again later.`
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(message));
+    })
+}
