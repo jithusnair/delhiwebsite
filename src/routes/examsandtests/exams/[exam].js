2d6f7cb91f79a3545bf747d273a69ce18aa9d4df
jithusnair <work.jithus@gmail.com>
2020-07-26 17:00:12 +0530

    new exams GET route

---
+++ src/routes/examsandtests/exams/[exam].js
@@ -0,0 +1,50 @@
+import Exam from '../../../_db/exam';
+
+export async function get(req, res, next) {
+    let message;
+
+    let {exam} = req.params;
+    
+    Exam.aggregate(
+        [
+            { $match : { examShortTitle : exam} },
+            { $unset : [ "sectorId",] } 
+        ])
+        .lookup({
+                from: "testpacks",
+                localField: "_id",
+                foreignField: "examId",
+                as: "testPacks"
+        })
+        .then((docs)=> {
+            // first remove all the links from the video details
+            if(!docs) {
+                let e = new Error('Cannot find the exam');
+                e.name = "CourseNullError";
+                throw e;
+            }
+            message = {success: true, data: docs[0],};
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(message));
+        })
+        .catch((error) => {
+            if(error.name == 'CourseNullError') {
+                message = { 
+                    success: false, 
+                    err: 
+                    `Could not find the exam.`
+                };
+            }
+            else {
+                console.log(error);
+                message = { 
+                    success: false, 
+                    serverErr: 
+                    `Something went wrong. Couldn't get videos from database! 
+                    Please try again later.`
+                };
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(message));
+        })
+}
