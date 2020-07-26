27ac8c22c7f84b0ab27f9e6ef9992744e53fe03d
jithusnair <work.jithus@gmail.com>
2020-07-26 17:02:43 +0530

    new component manager for tests-page elements

---
+++ src/components/tests/Manager.svelte
@@ -0,0 +1,56 @@
+<script>
+    import TestPacks from './TestPacks.svelte';
+    import Sectors from './Sectors.svelte';
+    import Exams from './Exams.svelte';
+</script>
+
+<style>
+    .container {
+        width: 90%;
+        margin: 1rem auto;
+        display: grid;
+        grid-template-columns: 20rem auto;
+        grid-template-rows: 5rem auto;
+        grid-template-areas: 
+        ". sectors"
+        "exams tests";
+    }
+
+    .sectors {
+        grid-area: sectors;
+    }
+
+    .exams {
+        grid-area: exams;
+    }
+
+    .tests {
+        grid-area: tests;
+    }
+  
+    @media screen and (max-width: 600px){
+        .container {
+            width: 95%;
+            margin: 1rem auto;
+            display: grid;
+            grid-template-columns: auto;
+            grid-template-rows: 5rem 5rem auto;
+            grid-template-areas: 
+            "sectors"
+            "exams"
+            "tests";
+        }
+    }
+</style>
+
+<div class="container">
+    <div class="sectors">
+        <Sectors/>
+    </div>
+    <div class="exams">
+        <Exams/>
+    </div>
+    <div class="tests">
+        <TestPacks/>
+    </div>
+</div>
