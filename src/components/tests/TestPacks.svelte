86b1a306485ee108ba5f6fc8b7717c701fdb0d7f
jithusnair <work.jithus@gmail.com>
2020-07-26 17:02:08 +0530

    new testpacks master component

---
+++ src/components/tests/TestPacks.svelte
@@ -0,0 +1,30 @@
+<script>
+    import TestPack from './TestPack.svelte';
+    import { testPacksDisplayStore } from '../../store/tests-controller.js';
+</script>
+
+<style>
+    .container {
+        margin-top: 2rem;
+        display: grid;
+        grid-gap: 5rem;
+        justify-content: center;
+        justify-items: center;
+        grid-template-columns: repeat(auto-fill, 300px);
+        grid-auto-flow: dense;
+    }
+
+    @media screen and (max-width: 600px){
+        .container {
+            margin-top: 5rem;
+        }
+    }
+</style>
+
+<div class="container">
+    {#if $testPacksDisplayStore && $testPacksDisplayStore.length != 0}
+        {#each $testPacksDisplayStore as testPack (testPack._id)}
+            <TestPack {testPack} />
+        {/each}
+    {/if}
+</div>
