119280584acce34e68bf5e4eee0e367bd265d2f6
jithusnair <work.jithus@gmail.com>
2020-08-10 23:50:49 +0530

    new testpacks display  components

---
+++ src/components/user/exam/TestPacks.svelte
@@ -0,0 +1,23 @@
+<script>
+    import TestPack from './TestPack.svelte';
+
+    export let testPackData;
+</script>
+
+<style>
+    .container {
+        margin-top: 2rem;
+        display: grid;
+        grid-gap: 5rem;
+        justify-content: center;
+        justify-items: center;
+        grid-template-columns: repeat(auto-fill, 300px);
+        grid-auto-flow: dense;
+    }
+</style>
+
+<div class="container">
+    {#each testPackData as testPack (testPack._id)}
+        <TestPack {testPack} />
+    {/each}
+</div>
