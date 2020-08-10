5fd1b61a29ee3e4c03ea0acd3c4ba91f7db5df40
jithusnair <work.jithus@gmail.com>
2020-08-10 23:51:33 +0530

    adds secondary language data fields to schema

--- src/_db/question.js
+++ src/_db/question.js
@@ -6,28 +6,52 @@ var QuestionSchema = new mongoose.Schema({
         ref: "Subject"
     },
     qnNumber: Number,
+    hindiVersion: {    // Indicates if hindi version of the question is present
+        type: Boolean,
+        default: false,
+    },
     comprehension: Boolean,
-    compImages: {
+    compImages: { // array with links to passage-images in S3
         type: [String],
         default: () => { return null; }
-    }, // array with links to passage-images in S3
+    }, 
     passageNumber: Number,
     passage: String, // should exist only if comprehension is true
+    passageHindi: {
+        type: String, 
+        default: null
+    },
     quesImages: {  // array with links to question-images in S3
         type: [String],
         default: () => { return null; }
     }, 
     question: String,
+    questionHindi: {
+        type: String, 
+        default: null
+    },
     optionsAreImages: Boolean, // indicates if options array contains image-links or text
     options: Array, // options parsed from html and saved as array
                     //or array with links to question-images in S3
+    optionsHindi: {
+        type: [String], 
+        default: () => { return null; }
+    },
     optionsHtml: String,
+    optionsHindiHtml: {
+        type: String, 
+        default: null
+    },
     correctAns: Number, // as the index of 'options'
     detailedAnsImages: { // array with links to question-images in S3
         type: [String],
         default: () => { return null; }
     },
     detailedAns: String,
+    detailedAnsHindi: {
+        type: String,
+        default: null,
+    }
 });
 
 module.exports = mongoose.model('Question', QuestionSchema);
