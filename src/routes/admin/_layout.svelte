eccefdc6bd8392844afcebc94c0b2ddc0684f208
jithusnair <work.jithus@gmail.com>
2020-08-10 23:57:26 +0530

    adds comments

--- src/routes/admin/_layout.svelte
+++ src/routes/admin/_layout.svelte
@@ -52,7 +52,7 @@
 </style>
 
 <!-- 
-	There is a weird html tag here.
+	There is a weird html tag conclosing script tag. ({@html '<scripit>......</script>'})
 	This is because of a bug in svelte:head tag. Without this @html it runs twice.
 	For scripts this can become a problem and this can break the functionality of 
 	the website.
@@ -61,6 +61,11 @@
 	for ref: 
 	https://github.com/sveltejs/sapper/issues/1124
 	https://github.com/sveltejs/svelte/issues/1607
+
+	Edit: The html tag has been removed. The bug still exists and the scripts are downloaded
+	twice. Previously it broke the trumboyng script. Now without this html it seems fine.
+ 
+	Edit 2: It breaks again, the HTML tag has been added again.
  -->
 
 <svelte:head>
@@ -69,10 +74,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.21.0/plugins/mathml/ui/trumbowyg.mathml.min.css">
 
     {@html '<script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.21.0/trumbowyg.min.js"></script>'}
-    {@html '<script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.19.1/plugins/table/trumbowyg.table.min.js"></script>'}
-	{@html '<script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.21.0/plugins/upload/trumbowyg.upload.min.js"></script>'}
-	{@html '<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML"></script>'}
-	{@html '<script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.21.0/plugins/mathml/trumbowyg.mathml.min.js"></script>'}
+    {@html '<script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.19.1/plugins/table/trumbowyg.table.min.js"></script>' }
+	{@html '<script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.21.0/plugins/upload/trumbowyg.upload.min.js"></script>' }
+	{@html '<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML"></script>' }
+	{@html '<script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.21.0/plugins/mathml/trumbowyg.mathml.min.js"></script>' }
 </svelte:head>
 
 
