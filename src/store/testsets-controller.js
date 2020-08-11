42c9351ca1d2ba05c57487708dfa81349f67117c
jithusnair <work.jithus@gmail.com>
2020-08-11 09:58:52 +0530

    new store to control testsets list page

---
+++ src/store/testsets-controller.js
@@ -0,0 +1,17 @@
+import { writable } from 'svelte/store';
+
+export let testPackStore = writable(0);
+
+export let sectionStore = writable(0);
+
+export let chapterStore = writable(0);
+
+export let testSetsStore = writable(0);
+
+export let selectedSectionStore = writable(0);
+
+export let selectedChapterStore = writable(0);
+
+export let chapterDisplayStore = writable(0);
+
+export let testSetsDisplayStore = writable(0);
