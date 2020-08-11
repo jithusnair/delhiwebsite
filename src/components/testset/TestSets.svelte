58bdb13675f05009bdc0a9724387333af77b3369
jithusnair <work.jithus@gmail.com>
2020-08-11 09:45:20 +0530

    new testsets controller componet

---
+++ src/components/testset/TestSets.svelte
@@ -0,0 +1,16 @@
+<script>
+    import TestSetDisplay from './TestSetDisplay.svelte';
+    import { testSetsDisplayStore } from '../../store/testsets-controller.js';
+
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+</script>
+
+{#if $testSetsDisplayStore && $testSetsDisplayStore.length != 0}
+    {#each $testSetsDisplayStore as testSet}
+        <TestSetDisplay {testSet} on:unlock/>
+    {/each}
+{:else}
+    <h3>No available tests</h3>
+{/if}
---
+++ src/components/user/testset/TestSets.svelte
@@ -0,0 +1,16 @@
+<script>
+    import TestSetDisplay from './TestSetDisplay.svelte';
+    import { testSetsDisplayStore } from '../../../store/testsets-controller-user.js';
+
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+</script>
+
+{#if $testSetsDisplayStore && $testSetsDisplayStore.length != 0}
+    {#each $testSetsDisplayStore as testSet}
+        <TestSetDisplay {testSet} on:unlock/>
+    {/each}
+{:else}
+    <h3>No available tests</h3>
+{/if}
