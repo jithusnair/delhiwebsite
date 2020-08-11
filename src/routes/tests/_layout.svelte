b01858df9fce3322a801ce8865c80b4cbadea435
jithusnair <work.jithus@gmail.com>
2020-08-11 09:58:28 +0530

    new layout to enable custom nav on [testpack] page

---
+++ src/routes/tests/_layout.svelte
@@ -0,0 +1,36 @@
+<script>
+	import NavPlusLoginSignup from '../../components/NavPlusLoginSignup.svelte';
+
+	export let segment;
+
+	let displayLogIn = false;
+	let displaySignUp = false;
+</script>
+
+<style>
+	div {
+		height: 100%;		
+	}
+</style>
+
+<div>
+	<slot></slot>
+</div>
+
+{#if segment === undefined}
+	<NavPlusLoginSignup
+		{displayLogIn}
+		{displaySignUp}
+		on:navlogin={() => displayLogIn = true}
+		on:signupopen = { () => {
+			displayLogIn = false;
+			displaySignUp = true;
+		}}
+		on:loginclose={() => displayLogIn = false}
+		on:loginopen = { () => {
+			displaySignUp = false;
+			displayLogIn = true;
+		}}
+		on:signupclose={() => displaySignUp = false}
+	/>
+{/if}
